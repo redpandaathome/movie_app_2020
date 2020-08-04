c8ff49fef6fa30f62b38d281039bb68c23408022
redpandaathome <leeyumi415@gmail.com>
2020-08-04 15:47:43 +0900

    Updates

---
+++ precache-manifest.daa136649aa4a8f878081c4b33b31c05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07748037fec63dfc2d4cabd4afa7543c",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "e4a73c6b6ed57a8eabf1",
+    "url": "/movie_app_2020/static/css/main.7748c369.chunk.css"
+  },
+  {
+    "revision": "3dd18e87ab088f0503d7",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4a73c6b6ed57a8eabf1",
+    "url": "/movie_app_2020/static/js/main.6e50f70d.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
