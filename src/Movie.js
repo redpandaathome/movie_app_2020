f3a4c7cd24a83002622a2f55f396753efb29a887
redpandaathome <leeyumi415@gmail.com>
2020-08-04 15:55:01 +0900

    Deploy on github

--- src/Movie.js
+++ src/Movie.js
@@ -12,8 +12,8 @@ function Movie({year, title, summary, poster, genres}){
             <div className="movie_data">
                 <h3 className="movie_title">{title}</h3>
                 <h5 className="move_year">{year}</h5>
-                <ul className="genres">{genres.map((genre, index) => <li key={index} className="genres_genre">{genre}</li>)}</ul>
-                <p className="movie_summary">{summary}</p>
+                <ul className="movie_genres">{genres.map((genre, index) => <li key={index} className="genres_genre">{genre}</li>)}</ul>
+                <p className="movie_summary">{summary.slice(0, 180)}...</p>
             </div>
          </div>
     );
